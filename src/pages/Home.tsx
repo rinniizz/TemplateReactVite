85565ba220becaa330efba0783c09b85dde12474
Jes <natdanai.pbm@gmail.com>
2025-11-23 16:59:21 +0700

    Update home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import Hero from '@components/sections/Hero'
-import ProductList from '@/components/sections/ProductList'
+// import ProductList from '@/components/sections/ProductList'
 import CTA from '@components/sections/CTA'
 
 const Home = () => {
   return (
     <div className="animate-fade-in">
       <Hero />
-     <div className="min-h-screen bg-gray-100">
+     {/* <div className="min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-center py-10">รายการสินค้า</h1>
       <ProductList />
-    </div>
+    </div> */}
       <CTA />
     </div>
   )
